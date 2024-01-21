e2ecd39e7ce8150655e695f1ef211659a3611381
ninianinia <nantadze086@gmail.com>
2024-01-21 15:16:19 +0300

    commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,37 @@
+name = "nika"
+#name 
+
+surname = "keshelava"
+
+print(name)
+
+# print(name)
+
+
+name = "nika"
+age = 14 
+height = 167 
+
+
+knows_programming = True
+is_ugly = False
+
+isUgly = False 
+
+
+print(name + " " + surname)
+
+print(name + age)
+# print(name + " " + surname
+
+
+# print(name + age)
+
+# print(type(age))
+# print(type(name))
+# print(type(surname))
+# print(type(height))
+# print(type(knows_programming))
+
+print(name + " " + str(age))
+
