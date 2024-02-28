c14fcc6d4d67523fbe79ee75edef1654955e9980
ninianinia <nantadze086@gmail.com>
2024-02-28 19:23:42 +0300

    commit

---
+++ day7/day7.py
@@ -0,0 +1,22 @@
+# სტრინგის ტესტი
+
+my_string = "hello"
+
+
+if isinstance(my_string, str):
+
+   print("my_string არის სტრინგი.")
+
+# ფლოატის ტესტი
+my_float = 5.5
+
+if isinstance(my_float, float)
+   
+   print("my float არის ფლოატი.")
+
+# ინტის ტესტი
+my_int = 10
+
+if isinstance(my_int, int):
+   
+   print("my_int არის მთელი რიცხვი.")
